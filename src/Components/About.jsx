a4affac41d12f17fbdcaa2a892987babf1335fd7
PriyadharshiniR <priyarama2209@gmail.com>
2026-06-19 15:14:46 +0530

    fixed name

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,7 @@ function About(){
         <section className="about" id="about" data-aos="fade-up">
             <h2>about me/</h2>
            
-            <p>I am a Computer Science Graduate who loves to solve problems and make the work efficient with the help of technology.I have solved 250+ Problems in Leetcode. I am passionate about Software Development.
+            <p>I am Priyadharshini I am a Computer Science Graduate who loves to solve problems and make the work efficient with the help of technology.I have solved 250+ Problems in Leetcode. I am passionate about Software Development.
                 I love to build cute little things using the techstacks I know.</p>
 
             <h3>Technologies I am working with:</h3> 
